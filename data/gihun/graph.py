02070da3ebe898529023da06db4730dd435630ae
kgh1997 <gihungim709@gmail.com>
2023-02-19 15:40:04 +0900

    Nine Modified

---
+++ data/gihun/graph.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import numpy as np
+from catboost import CatBoostRegressor
+import matplotlib.pyplot as plt
+
+def get_graph(model,importances, X):
+    std = np.std([tree.feature_importances_ for tree in model.estimators_],
+                 axis=0)
+                 
+    indices = np.argsort(importances)[::-1]
+
+    # Print the feature ranking
+    print("Feature ranking:")
+
+    for f in range(X.shape[1]):
+        print("%d. feature %d (%f)" % (f + 1, indices[f], importances[indices[f]]))
+
+    # Plot the feature importances of the forest
+    plt.figure()
+    plt.title("Feature importances")
+    plt.bar(range(X.shape[1]), importances[indices],
+           color="r", yerr=std[indices], align="center")
+    plt.xticks(range(X.shape[1]), indices)
+    plt.xlim([-1, X.shape[1]])
+    plt.show()
